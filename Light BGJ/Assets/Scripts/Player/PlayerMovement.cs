b08f3557cdc9eab147ca38102f712a0a800c5161
Nnamelhu <TeufelsDoener@gmail.com>
2018-08-25 23:00:42 +0200

    Added PlayerMovement
    added KeyInput
    addad movement

---
+++ Light BGJ/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float acceleration;
+    public float maxspeed;
+    private Rigidbody2D rb2d;
+   
+	// Use this for initialization
+	void Start (){ 
+        // get Rigidbody2D component
+        rb2d = GetComponent<Rigidbody2D>();  
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() { 
+        // get Key-Input 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector2 movement = new Vector2(horizontal, vertical); 
+        // apply limited force 
+        if (rb2d.velocity.magnitude <= maxspeed) {
+            rb2d.AddForce(movement * acceleration);
+        }
+
+       
+    }
+
+}
