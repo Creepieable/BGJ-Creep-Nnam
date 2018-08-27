dedde4eb1d33b98eb8cdadf4120b3ede381375ee
Nnamelhu <TeufelsDoener@gmail.com>
2018-08-27 21:12:03 +0200

    Menu and HUD Changes
    added MenuManager
    deleted QuitGame script
    deleted LoadSceneonClick script

---
+++ Light BGJ/Assets/Scripts/Main Menu/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuManager : MonoBehaviour {
+    
+
+    public void quit_game()
+    {
+        Debug.Log("You have quit the game!");
+        Application.Quit();
+    }
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
