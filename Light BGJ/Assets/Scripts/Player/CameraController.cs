23a6a40d1f084c9dd388f2100a84ae93e1b11d12
Creepieable <38226503+jojoban999@gmail.com>
2018-08-27 18:08:33 +0200

    added camera controll
    added CammeraController in Scripts/Player folder

---
+++ Light BGJ/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform Player;
+    public float lerpTime = 0.05f;
+
+    private Vector3 offset;
+    private float trueLerpTime;
+
+	void Awake () {
+
+        offset = new Vector3(0,0,transform.position.z);
+
+        if(Player == null)
+        {
+            Debug.LogWarning("No Player transform in CameraController.");
+        }
+	}
+	
+
+	void FixedUpdate () {
+
+        if (Player != null)
+        {
+
+            trueLerpTime = lerpTime * Vector3.Distance(Player.position, transform.position - offset);
+
+            transform.position = Vector3.Lerp(transform.position, Player.position + offset, lerpTime);
+
+        }
+
+	}
+}
