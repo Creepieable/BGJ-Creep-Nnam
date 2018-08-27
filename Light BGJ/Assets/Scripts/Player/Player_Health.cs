9e73a3e213ce0b3952bd87ef5ac033e84b869c34
Nnamelhu <TeufelsDoener@gmail.com>
2018-08-27 12:34:13 +0200

    added Player Health
    added Player Health
    added Player Death
    added repulsion

---
+++ Light BGJ/Assets/Scripts/Player/Player_Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour {
+    public int player_hp;
+    public float magnitude;
+    private void OnCollisionEnter2D(Collision2D collision) { 
+        if (collision.gameObject.tag == "Enemy")
+        {
+            player_hp--;
+            var force = transform.position - collision.transform.position;
+            force.Normalize();
+            gameObject.GetComponent<Rigidbody2D>().AddForce(force * magnitude);
+
+
+
+
+        }
+
+        if (player_hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    
+        
+    }
+}
