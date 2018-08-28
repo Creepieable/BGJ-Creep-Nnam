629ae9600d1e02a80993903e384ff9d0e525bf07
Creepieable <38226503+jojoban999@gmail.com>
2018-08-28 19:49:00 +0200

    improved playerheringscript

--- Light BGJ/Assets/Scripts/Enemy/EnemySoundDetect.cs
+++ Light BGJ/Assets/Scripts/Enemy/EnemySoundDetect.cs
@@ -8,15 +8,15 @@ public class EnemySoundDetect : MonoBehaviour {
     public LayerMask layer;
     private GameObject player;
 
-    public float maxTimeToMakeaSound;
     public float minTimeToMakeaSound;
+    public float maxTimeToMakeaSound;
     private float timeToMakeaSound;
     private float timer;
 
-    [Range(0, 10)]
-    public float maxOffset;
-    [Range(0, -10)]
+    [Range(0,10)]
     public float minOffset;
+    [Range(0,10)]
+    public float maxOffset;
 
     [Tooltip("List of Prefabs to instatiate near enemy position.")]
     public GameObject[] effects;
@@ -32,7 +32,7 @@ public class EnemySoundDetect : MonoBehaviour {
         {
             if (timer > timeToMakeaSound)
             {
-                Vector3 randOffset = new Vector3(Random.Range(minOffset,maxOffset), Random.Range(minOffset, maxOffset), transform.position.z);
+                Vector3 randOffset = new Vector3(RandomOffset(), RandomOffset(), transform.position.z);
                 Quaternion randRot = new Quaternion(transform.rotation.x, transform.rotation.y, Random.rotation.z, transform.rotation.w);
                 GameObject instance = Instantiate(effects[Random.Range(0, effects.Length)], transform.position + randOffset, randRot) as GameObject;
 
@@ -52,5 +52,17 @@ public class EnemySoundDetect : MonoBehaviour {
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, loudness);
+    }
+
+    float RandomOffset()
+    {
+        float rnd = Random.Range(minOffset, maxOffset);
+
+        if (Random.Range(0, 1) == 1)
+        {
+            rnd -= rnd * 2;
+        }
+
+        return rnd;
     }
 }
