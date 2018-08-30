44a0917acdde6ef9c5de936c5d1a5488bbaa89e2
Nnamelhu <TeufelsDoener@gmail.com>
2018-08-30 22:08:08 +0200

    added Bow
    added PlayerAttack script
    added Arrow script

--- Light BGJ/Assets/Scripts/Items/Arrow.cs
+++ Light BGJ/Assets/Scripts/Items/Arrow.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Arrow : MonoBehaviour {
+    public float multiplier;
 
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float standardspeed;
+    public float realspeed;
+    
+    private Vector2 target;
+
+    
+
+
+    private void Start()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        multiplier = GameObject.Find("Player").GetComponent<PlayerAttack>().tightenTime;
+        
+
+    }
+
+
+    void Update () {
+        realspeed = standardspeed * multiplier;
+        transform.position = Vector2.MoveTowards(transform.position, target, realspeed* Time.deltaTime);
+    }
+
+   
 }
+
--- Light BGJ/Assets/Scripts/Player/PlayerAttack.cs
+++ Light BGJ/Assets/Scripts/Player/PlayerAttack.cs
@@ -3,31 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
+    public GameObject arrow;
 
-    public bool take = false;
-    private void OnCollisionEnter2D(Collision2D collision)
+    public float timeBetweenShots;
+    public float shotCounter;
+    public float tightenTime;
+
+    public int arrows;
+    
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
     {
-        if (collision.gameObject.tag == "Item")
-        {
-            gameObject.transform.parent = collision.gameObject.transform;
-            take = true;
-
-        }
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-
-
-
-    void Update()
+    private void Update()
     {
-        if (take == true)
+        if (shotCounter > 0)
         {
-            attack();
+           shotCounter -= Time.deltaTime;
+        }
+        
+       if (arrows > 0 && shotCounter<=0 )
+        {
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+
+               while(Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    tightenTime += Time.deltaTime;
+                }
+
+                if (Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    attack();
+                }
+                
+                
+            }
+            
+        } 
+       if (tightenTime > 3)
+        {
+            tightenTime = 3;
         }
     }
-        void attack() { 
-        if (Input.GetKeyDown(KeyCode.Space)){
-            Destroy(gameObject);
-        }
+
+
+    void attack()
+    {
+        arrows--;
+        shotCounter = timeBetweenShots;
+        Instantiate(arrow, rb2d.position, Quaternion.identity);
+
     }
+
+
+
+
 }
