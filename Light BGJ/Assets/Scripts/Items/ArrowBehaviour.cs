80fc66c8dc37bd47f1538cb1799d553455545644
Creepieable <38226503+jojoban999@gmail.com>
2018-09-01 01:39:05 +0200

    45698

--- Light BGJ/Assets/Scripts/Items/ArrowBehaviour.cs
+++ Light BGJ/Assets/Scripts/Items/ArrowBehaviour.cs
@@ -9,14 +9,23 @@ public class ArrowBehaviour : MonoBehaviour {
     [HideInInspector]
     public Vector2 startPos;
 
+    private int rotated = 0;
+
     private Rigidbody2D rb2d;
 
     private void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();       
     }
 
     void Update () {
+
+        if (rotated < 10)
+        {
+            float rot = Mathf.Atan2(rb2d.velocity.normalized.x, rb2d.velocity.normalized.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot);
+            rotated++;
+        }
 
         if (Vector2.Distance(startPos, transform.position) >= flightDist)
         {
